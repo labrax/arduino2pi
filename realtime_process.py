a6cd7793ba7abe4c55a0764b1a2fdd11176b7562
Victor <labrax@gmail.com>
2019-01-07 22:42:30 +0000

    using a created class to store the values rather than a list

--- realtime_process.py
+++ realtime_process.py
@@ -1,5 +1,25 @@
+
+
 import serial
 import time
+
+class MinMaxSum:
+    def __init__(self):
+        self.reset()
+    def reset(self):
+        self.min = 999999999
+        self.max = -999999999
+        self.sum = 0
+        self.len = 0
+    def add(self, n):
+        if n < self.min:
+            self.min = n
+        if n > self.max:
+            self.max = n
+        self.sum += n
+        self.len += 1
+
+        
 
 #startup
 filename = '/home/pi/arduino2pi/data/' + time.strftime("%Y%m%d") + '_v2.csv'
@@ -20,7 +40,7 @@ def output(s):
 
 def run():
     ser = serial.Serial('/dev/ttyUSB0',9600)
-    PIR_LIST, MIC_V_LIST, MIC_C_LIST, PHR_S_LIST, BMP_T_LIST, BMP_P_LIST = [], [], [], [], [], []
+    PIR_LIST, MIC_V_LIST, MIC_C_LIST, PHR_S_LIST, BMP_T_LIST, BMP_P_LIST = MinMaxSum(), MinMaxSum(), MinMaxSum(), MinMaxSum(), MinMaxSum(), MinMaxSum()
     workingwith = time.strftime("%S")
     while True:
         read_serial = ser.readline()
@@ -29,37 +49,38 @@ def run():
         if len(E) < 6:
             continue
         _, PIR, MIC_V, MIC_C, PHR_S, BMP_T, BMP_P = E
-        PIR_LIST.append(int(PIR))
-        MIC_V_LIST.append(float(MIC_V))
-        MIC_C_LIST.append(int(MIC_C))
-        PHR_S_LIST.append(int(PHR_S))
-        BMP_T_LIST.append(float(BMP_T))
-        BMP_P_LIST.append(float(BMP_P))
+        PIR_LIST.add(int(PIR))
+        MIC_V_LIST.add(float(MIC_V))
+        MIC_C_LIST.add(int(MIC_C))
+        PHR_S_LIST.add(int(PHR_S))
+        BMP_T_LIST.add(float(BMP_T))
+        BMP_P_LIST.add(float(BMP_P))
         cur = time.strftime("%S")
         if cur != workingwith:
-            Plen = len(PIR_LIST)
+            Plen = PIR_LIST.len
             if Plen > 0:
-                Pmin = min(PIR_LIST)
-                Pmax = max(PIR_LIST)
-                Psum = sum(PIR_LIST)
-                MVmin = min(MIC_V_LIST)
-                MVmax = max(MIC_V_LIST)
-                MVsum = sum(MIC_V_LIST)
+                Pmin = PIR_LIST.min
+                Pmax = PIR_LIST.max
+                Psum = PIR_LIST.sum
+                MVmin = MIC_V_LIST.min
+                MVmax = MIC_V_LIST.max
+                MVsum = MIC_V_LIST.sum
                 #MCmin = min(MIC_C_LIST)
                 #MCmax = max(MIC_C_LIST)
                 #MCsum = sum(MIC_C_LIST)
-                PHRmin = min(PHR_S_LIST)
-                PHRmax = max(PHR_S_LIST)
-                BTmin = min(BMP_T_LIST)
-                BTmax = max(BMP_T_LIST)
-                BTsum = sum(BMP_T_LIST)
-                BPmin = min(BMP_P_LIST)
-                BPmax = max(BMP_P_LIST)
-                BPsum = sum(BMP_P_LIST)
+                PHRmin = PHR_S_LIST.min
+                PHRmax = PHR_S_LIST.max
+                BTavg = BMP_T_LIST.sum/Plen
+                BPavg = BMP_P_LIST.sum/Plen
                 #PHRsum = sum(PHR_S_LIST)
-                acc = ','.join([str(round(i, 2)) for i in [Plen, Pmin, Pmax, Psum, MVmin, MVmax, MVsum, PHRmin, PHRmax, BTmin, BTmax, BTsum, BPmin, BPmax, BPsum]])
+                acc = ','.join([str(round(i, 2)) for i in [Plen, Pmin, Pmax, Psum, MVmin, MVmax, MVsum, PHRmin, PHRmax, BTavg, BPavg]])
                 output(acc)
-            PIR_LIST, MIC_V_LIST, MIC_C_LIST, PHR_S_LIST, BMP_T_LIST, BMP_P_LIST = [], [], [], [], [], []
+            PIR_LIST.reset()
+            MIC_V_LIST.reset()
+            MIC_C_LIST.reset()
+            PHR_S_LIST.reset()
+            BMP_T_LIST.reset()
+            BMP_P_LIST.reset()
             workingwith = cur
 
 
@@ -68,6 +89,7 @@ if __name__ == '__main__':
         try:
             run()
             time.sleep(5)
-        except:
-            pass
+        except Exception as e:
+            print(e)
+
 
