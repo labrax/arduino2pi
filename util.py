60f9c59f6df8890af5793f55afbdc57fa7a3ddf6
Victor <labrax@gmail.com>
2019-05-06 17:06:22 +0100

    influx send data

--- util.py
+++ util.py
@@ -8,13 +8,12 @@ class InfluxHandler:
         self.database = database
         self.table = table
         self.host = host_sender
-        self.url_string = 'http://{address}/write?db={database}'.format(self.address, self.database)
+        self.url_string = 'http://{address}/write?db={database}'.format(address=self.address, database=self.database)
 
     def _formatMessage(self, sensors):
-        return '{table_name},host={host} {message}'.format(table_name=self.table, host=self.host, message=','.join(["{}={}" for i, j in sensors]))
+        return '{table_name},host={host} {message}'.format(table_name=self.table, host=self.host, message=','.join(["{}={}".format(i, j) for i, j in sensors.items()]))
 
     def sendInflux(self, sensors):
-        print(self.url_string, _formatMessage(sensors))
-        #return requests.post(self.url_string, data=_formatMessage(sensors))
-
+        #print(self.url_string, self._formatMessage(sensors))
+        return requests.post(self.url_string, data=self._formatMessage(sensors))
 
